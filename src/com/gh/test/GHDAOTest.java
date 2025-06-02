e988b988f873b29ecf4f56395cd3ba171fa6b98f
sunhokim1 <92263333+sunhokim1@users.noreply.github.com>
2025-06-02 16:49:30 +0900

    feat : test connect drive
    드라이브 연결

--- src/com/gh/test/GHDAOTest.java
+++ src/com/gh/test/GHDAOTest.java
@@ -1,7 +1,19 @@
 package com.gh.test;
 
+import java.sql.DriverManager;
+
+import config.ServerInfo;
+
 public class GHDAOTest {
 	public static void main(String[] args) {
 		
+	}
+	
+	static {
+		try {
+			Class.forName(ServerInfo.DRIVER_NAME);
+		}catch(ClassNotFoundException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
