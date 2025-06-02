daa251827bf8b00bcff51e01b0e332202c704d34
sunhokim1 <92263333+sunhokim1@users.noreply.github.com>
2025-06-02 16:53:58 +0900

    feat : connect, closeAll
    커넥션과 자원반환 공통로직 작성

--- src/com/gh/dao/impl/GHDAOImpl.java
+++ src/com/gh/dao/impl/GHDAOImpl.java
@@ -1,7 +1,29 @@
 package com.gh.dao.impl;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import com.gh.dao.GHDAO;
 
-public class GHDAOImpl implements GHDAO {
+import config.ServerInfo;
 
+public class GHDAOImpl implements GHDAO {
+	//공통로직
+	private Connection getConnect() throws SQLException {
+		Connection conn = DriverManager.getConnection(ServerInfo.URL, ServerInfo.USER, ServerInfo.PASS);
+		return conn;
+	}
+	
+	private void closeAll(PreparedStatement ps, Connection conn) throws SQLException {
+		if(ps != null) ps.close();
+		if(conn != null) conn.close();
+	}
+	
+	private void closeAll(ResultSet rs, PreparedStatement ps, Connection conn) throws SQLException {
+		if(rs != null) rs.close();
+		closeAll(ps, conn);
+	}
 }
