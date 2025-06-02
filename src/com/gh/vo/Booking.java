aaefd1764f9b7a237e181538d72f9fd6721906ae
opla83 <ehddbs9252@gmail.com>
2025-06-02 16:59:01 +0900

    booking vo클래스 작성

--- src/com/gh/vo/Booking.java
+++ src/com/gh/vo/Booking.java
@@ -1,5 +1,77 @@
 package com.gh.vo;
 
 public class Booking {
+	private String bookingId;
+	private int peopleCount;
+	private String checkIn;
+	private int nights;
+	private String bookingStatus;
+	private int totalPrice;
+	public Booking() {
+	}
+	
+	public Booking(String bookingId, int peopleCount, String checkIn, int nights, String bookingStatus, int totalPrice) {
+		super();
+		this.bookingId = bookingId;
+		this.peopleCount = peopleCount;
+		this.checkIn = checkIn;
+		this.nights = nights;
+		this.bookingStatus = bookingStatus;
+		this.totalPrice = totalPrice;
+	}
+	
+	public String getBookingId() {
+		return bookingId;
+	}
 
+	public void setBookingId(String bookingId) {
+		this.bookingId = bookingId;
+	}
+
+	public int getPeopleCount() {
+		return peopleCount;
+	}
+
+	public void setPeopleCount(int peopleCount) {
+		this.peopleCount = peopleCount;
+	}
+
+	public String getCheckIn() {
+		return checkIn;
+	}
+
+	public void setCheckIn(String checkIn) {
+		this.checkIn = checkIn;
+	}
+
+	public int getNights() {
+		return nights;
+	}
+
+	public void setNights(int nights) {
+		this.nights = nights;
+	}
+
+	public String getBookingStatus() {
+		return bookingStatus;
+	}
+
+	public void setBookingStatus(String bookingStatus) {
+		this.bookingStatus = bookingStatus;
+	}
+
+	public int getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(int totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	@Override
+	public String toString() {
+		return "Booking [bookingId=" + bookingId + ", peopleCount=" + peopleCount + ", checkIn=" + checkIn + ", nights=" + nights
+				+ ", bookingStatus=" + bookingStatus + ", totalPrice=" + totalPrice + "]";
+	}
+	
 }
