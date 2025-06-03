dff358913e09e237040339a1aecaae72e4002226
JunYong <chary8028@naver.com>
2025-06-03 15:57:52 +0900

    Feat: interface 추가

--- src/com/gh/dao/GHDAO.java
+++ src/com/gh/dao/GHDAO.java
@@ -1,5 +1,37 @@
 package com.gh.dao;
 
-public interface GHDAO {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Map;
 
+import com.gh.vo.Booking;
+import com.gh.vo.Client;
+import com.gh.vo.Guesthouse;
+
+public interface GHDAO {
+	
+	/*
+	 * Client
+	 */
+	double calcDiscountByTier(Client c);
+	boolean canBook(Guesthouse gh, LocalDate checkIn, int nights, int people);
+	int getDayBetweenBooking(LocalDate previousCheckIn);
+	void login(String id, String password);
+	void logout();
+	void reserveBooking(Client client, Booking booking);
+	ArrayList<Guesthouse> recommendGH(int price, char mbti);
+	void printMyInfo(Client c);
+	
+	/*
+	 * Manager
+	 */
+	Map<String, Integer> getWeeklyVisitorCount(LocalDate checkIn, LocalDate checkOut);
+	Map<String, Integer> getWeeklySales(LocalDate checkIn, LocalDate checkOut);
+	Guesthouse getMostBookedGH(LocalDate checkIn, LocalDate checkOut);
+	double calAverageVisitInterval(Client client);
+	Map<String, Double> calAverageStayByTier();
+	double calCancelRate();
+	Client getClientById(String id);
+	ArrayList<Client> getAllClients();
+	ArrayList<Booking> getAllBookings();
 }
