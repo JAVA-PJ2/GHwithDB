1d32a82ac198119004de2e284e8402f7256add9e
소유나 <93272421+seacrab808@users.noreply.github.com>
2025-06-04 09:27:33 +0900

    add unimplement method

--- src/com/gh/dao/impl/GHDAOImpl.java
+++ src/com/gh/dao/impl/GHDAOImpl.java
@@ -5,8 +5,14 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Map;
 
 import com.gh.dao.GHDAO;
+import com.gh.vo.Booking;
+import com.gh.vo.Client;
+import com.gh.vo.Guesthouse;
 
 import config.ServerInfo;
 
@@ -41,5 +47,107 @@ public class GHDAOImpl implements GHDAO {
 	private void closeAll(ResultSet rs, PreparedStatement ps, Connection conn) throws SQLException {
 		if(rs != null) rs.close();
 		closeAll(ps, conn);
+	}
+
+	@Override
+	public double calcDiscountByTier(Client c) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean canBook(Guesthouse gh, LocalDate checkIn, int nights, int people) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getDayBetweenBooking(LocalDate previousCheckIn) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void login(String id, String password) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void logout() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reserveBooking(Client client, Booking booking) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<Guesthouse> recommendGH(int price, char mbti) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void printMyInfo(Client c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Map<String, Integer> getWeeklyVisitorCount(LocalDate checkIn, LocalDate checkOut) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, Integer> getWeeklySales(LocalDate checkIn, LocalDate checkOut) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Guesthouse getMostBookedGH(LocalDate checkIn, LocalDate checkOut) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double calAverageVisitInterval(Client client) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Map<String, Double> calAverageStayByTier() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double calCancelRate() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Client getClientById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Client> getAllClients() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Booking> getAllBookings() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
